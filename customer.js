47d074cf8a7539d3e8cc850aa398aa39ffecea66
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-04-21 14:41:14 -0700

    fixed data seed

--- customer.js
+++ customer.js
@@ -107,12 +107,15 @@ function askAgain() {
 }
 
 // *** CALLS ***
+// seed product data
+cmd.run('mysql -u root < inventory.sql');
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-    if (err) throw err;
-    // seed product data
-    cmd.run('mysql -u root < inventory.sql');
-    // run the start function after the connection is made to prompt the user
-    start();
-});
+setTimeout(() => {
+        connection.connect(function(err) {
+            if (err) throw err;
+            // run the start function after the connection is made to prompt the user
+            start();
+        });
+    },
+    100);
